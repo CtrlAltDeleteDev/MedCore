f9a0b898a72ebd870744d78d17a735d5fb9ddd71
hentax <>
2026-04-22 13:27:49 +0300

    identity server

---
+++ services/MedCore.Auth/MedCore.Auth.IdentityServer/Pages/Account/Logout/LogoutOptions.cs
@@ -0,0 +1,7 @@
+namespace MedCore.Auth.IdentityServer.Pages.Account.Logout;
+
+public static class LogoutOptions
+{
+    public static readonly bool ShowLogoutPrompt = true;
+    public static readonly bool AutomaticRedirectAfterSignOut = false;
+}
