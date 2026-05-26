f06f96b9e39ff1f4138a7f4c19ca437d848044e2
hentax <>
2026-05-26 19:11:14 +0300

    upd3

--- services/MedCore.Appointment/MedCore.Appointment.Api/Program.cs
+++ services/MedCore.Appointment/MedCore.Appointment.Api/Program.cs
@@ -29,13 +29,12 @@ builder.Services.AddSwaggerGen(options =>
 {
     options.SwaggerDoc("v1", new OpenApiInfo { Title = "MedCore Appointment API", Version = "v1" });
 
-    options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+    options.AddSecurityDefinition("bearer", new OpenApiSecurityScheme
     {
-        Name = "Authorization",
         Type = SecuritySchemeType.Http,
         Scheme = "bearer",
         BearerFormat = "JWT",
-        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme."
     });
 
     options.AddSecurityRequirement(document => new OpenApiSecurityRequirement
