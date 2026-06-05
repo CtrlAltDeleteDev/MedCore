38b15324ad0256d5a7cd2fb83e8c229769015b9b
hentax <>
2026-06-05 15:33:40 +0300

    repos

--- services/MedCore.Appointment/MedCore.Appoitment.Infrastura/Repositories/EmployeeRepository.cs
+++ services/MedCore.Appointment/MedCore.Appoitment.Infrastura/Repositories/EmployeeRepository.cs
@@ -5,24 +5,36 @@ using MedCore.Appoitment.Data.Repositories;
 using MedCore.Appoitment.Data.Specifications;
 using Microsoft.EntityFrameworkCore;
 
-namespace MedCore.Appoitment.Infrastura.Repositories;
+namespace MedCore.Appoitment.Infrastructure.Repositories;
 
-public class EmployeeRepository(AppoitmentDbContext dbContext) : IRepository<Employee,EmployeeDto>
+public class EmployeeRepository(AppoitmentDbContext dbContext) : IRepository<Employee>
 {
     private readonly AppoitmentDbContext _dbContext = dbContext;
 
-    public async Task<IEnumerable<EmployeeDto>> GetItemsAsync(ISpecification<Employee> spec, CancellationToken cancellationToken = default)
+    public async Task<IEnumerable<Employee>> GetItemsAsync(ISpecification<Employee> spec, bool useAsNoTracking = false,  CancellationToken cancellationToken = default)
     {
-        return await _dbContext.Employees
-            .Where(spec.Criteria)
-            .Select(x => new EmployeeDto(x.Id, x.FullName, x.Title, x.Skills
-                .Select(s => s.Id)
-                .ToArray())).ToListAsync(cancellationToken);
+        var query = _dbContext.Employees
+            .Where(spec.Criteria);
+
+        return await (useAsNoTracking ? query.AsNoTracking() : query).ToListAsync(cancellationToken);            
     }
 
     public async Task Add(Employee entity,  CancellationToken cancellationToken = default)
     {
         _dbContext.Employees.Add(entity);
         await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task Update(Employee entity, CancellationToken cancellationToken = default)
+    {
+        _dbContext.Entry(entity).State = EntityState.Modified;
+        await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task<Employee> GetItemAsync(ISpecification<Employee> spec, bool useAsNoTracking = false, CancellationToken cancellationToken = default)
+    {
+        var query = _dbContext.Employees;
+
+        return await (useAsNoTracking ? query.AsNoTracking() : query).FirstOrDefaultAsync(spec.Criteria, cancellationToken);
     }
 }
--- services/MedCore.Appointment/MedCore.Appoitment.Infrastura/Repositories/MeetRepository.cs
+++ services/MedCore.Appointment/MedCore.Appoitment.Infrastura/Repositories/MeetRepository.cs
@@ -5,23 +5,35 @@ using MedCore.Appoitment.Data.Repositories;
 using MedCore.Appoitment.Data.Specifications;
 using Microsoft.EntityFrameworkCore;
 
-namespace MedCore.Appoitment.Infrastura.Repositories;
+namespace MedCore.Appoitment.Infrastructure.Repositories;
 
-public class MeetRepository(AppoitmentDbContext dbContext): IRepository<Meet, MeetDto>
+public class MeetRepository(AppoitmentDbContext dbContext): IRepository<Meet>
 {
     private readonly AppoitmentDbContext _dbContext = dbContext;
-    public async Task<IEnumerable<MeetDto>> GetItemsAsync(ISpecification<Meet> spec, CancellationToken cancellationToken = default)
+    public async Task<IEnumerable<Meet>> GetItemsAsync(ISpecification<Meet> spec, bool useAsNoTracking = false, CancellationToken cancellationToken = default)
     {
-        return await _dbContext.Meets
-            .Where(spec.Criteria)
-            .Select(x => new MeetDto(x.Id, x.Subject, x.StartTime, x.EndTime, x.EmployeeId, x.PatientId, x.SkillIds))
-            .ToListAsync(cancellationToken);
-
+        var query = _dbContext.Meets
+            .Where(spec.Criteria);
+            
+        return await (useAsNoTracking ? query.AsNoTracking() : query).ToListAsync(cancellationToken);
     }
 
     public async Task Add(Meet entity, CancellationToken cancellationToken = default)
     {
         _dbContext.Meets.Add(entity);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    public async  Task<Meet> GetItemAsync(ISpecification<Meet> spec, bool useAsNoTracking = false, CancellationToken cancellationToken = default)
+    {
+        var query = _dbContext.Meets;
+
+        return await (useAsNoTracking ? query.AsNoTracking() : query).FirstOrDefaultAsync(spec.Criteria, cancellationToken);
+    }
+
+    public async Task Update(Meet entity, CancellationToken cancellationToken = default)
+    {
+        _dbContext.Entry(entity).State = EntityState.Modified;
         await _dbContext.SaveChangesAsync(cancellationToken);
     }
 }
